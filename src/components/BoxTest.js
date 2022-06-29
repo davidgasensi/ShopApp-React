7041eff97498843ba15d4866ac2202f8118c2334
David Asensi <davidgasensi@gmail.com>
2022-06-29 19:34:19 +0200

    box test

--- src/components/BoxTest.js
+++ src/components/BoxTest.js
@@ -1,61 +1,148 @@
 import { Box, Image, Badge, Center } from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
 function BoxTest() {
-  const property = {
-    imageUrl: "https://bit.ly/2Z4KKcF",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  };
+  const property = [
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Pruebita",
+      beds: 3,
+      baths: 2,
+      title: "Pruebazoto",
+      formattedPrice: "$5,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    {
+      imageUrl: "https://bit.ly/2Z4KKcF",
+      imageAlt: "Rear view of modern home with pool",
+      beds: 3,
+      baths: 2,
+      title: "Modern home in city center in the heart of historic Los Angeles",
+      formattedPrice: "$1,900.00",
+      reviewCount: 34,
+      rating: 4,
+    },
+    
+    
+  ];
 
   return (
-    <Center>
-    <Box maxW="sm" borderWidth="1px" overflow='hidden' boxShadow='lg' rounded='md'>
-      <Image src={property.imageUrl} alt={property.imageAlt} />
-      <Box p="6">
-        <Box display='flex' alignItems='baseline'>
-          <Badge borderRadius="full" px="2" colorScheme="red">
-            New
-          </Badge>
-          <Box
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            textTransform="uppercase"
-            ml="2"
-          >
-            {property.beds} beds &bull; {property.baths} baths
+    <Center display="flex" flexWrap="wrap">
+      {property.map((property) => (
+      <Box
+        w='20rem'
+        h='24rem'
+        borderWidth="1px"
+        overflow="hidden"
+        boxShadow="lg"
+        rounded="md"
+        mr="5"
+        mb="5"
+      >
+        
+        <Image src={property.imageUrl} alt={property.imageAlt} />
+        <Box p="6">
+          <Box display="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="red">
+              New
+            </Badge>
+            <Box
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              ml="2"
+            >
+              {property.beds} beds &bull; {property.baths} baths
+            </Box>
           </Box>
-        </Box>
-        <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' ml='2'>
+          <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" ml="2">
             {property.title}
-        </Box>
-        <Box ml='2'>
+          </Box>
+          <Box ml="2">
             {property.formattedPrice}
-            <Box as='span' color='gray.600' fontSize='sm'>
-            / wk
+            <Box as="span" color="gray.600" fontSize="sm">
+              / wk
+            </Box>
+          </Box>
+          <Box display="flex" mt="2" alignItems="center">
+            {Array(5)
+              .fill("")
+              .map((_, i) => (
+                <StarIcon
+                  key={i}
+                  color={i < property.rating ? "teal.500" : "gray.300"}
+                />
+              ))}
+            <Box as="span" ml="2" color="gray.600" fontSize="sm">
+              {property.reviewCount} reviews
+            </Box>
           </Box>
         </Box>
-        <Box display='flex' mt='2' ml='20' alignItems='center'>
-          {Array(5)
-            .fill('')
-            .map((_, i) => (
-              <StarIcon
-                key={i}
-                color={i < property.rating ? 'teal.500' : 'gray.300'}
-              />
-            ))}
-          <Box as='span' ml='2' color='gray.600' fontSize='sm'>
-            {property.reviewCount} reviews
-          </Box>
-        </Box>
+        
       </Box>
-    </Box>
+      ))}
     </Center>
   );
 }
